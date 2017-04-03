9c02cb3f0dd400bee28f5edca4e50daa737105ed
Alex Cahn <alexcahn@gmail.com>
2017-04-03 12:06:41 -0700

    got some github oauth stuff setup

---
+++ GoGoGithub/GoGoGithub/GitHub.swift
@@ -0,0 +1,64 @@
+//
+//  GitHub.swift
+//  GoGoGithub
+//
+//  Created by A Cahn on 4/3/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+import UIKit
+
+let kOAuthBaseURLString = "https://github.com/login/oauth/"
+
+enum GitHubAuthError: Error {
+    case extractingCode
+}
+
+class GitHub {
+    
+    let gitHubClientID = "b3dd1203cd38fe1a3e8f"
+    let gitHubClientSecret = "b2875f0c807f43b73a95dc6913f27f3187b09fe5"
+    
+    static let shared = GitHub()
+    
+    func oAuthRequestWith(parameters: [String : String]) {
+        var parametersString = ""
+        
+        for (key, value) in parameters {
+            parametersString += "&\(key)=\(value)"
+        }
+        
+        print("Parameter String: \(parametersString)")
+        
+        if let requestURL = URL(string: "\(kOAuthBaseURLString)authorize?client_id=\(gitHubClientID)\(parametersString)") {
+            
+            print(requestURL.absoluteString)
+            
+            UIApplication.shared.open(requestURL)
+        }
+    }
+    
+    func getCodeFrom(url: URL) throws -> String {
+        
+        guard let code = url.absoluteString.components(separatedBy: "=").last else {
+            throw GitHubAuthError.extractingCode
+        }
+        
+        return code
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GoGoGithub/GoGoGithub/GitHubAuthController.swift
@@ -0,0 +1,33 @@
+//
+//  GitHubAuthController.swift
+//  GoGoGithub
+//
+//  Created by A Cahn on 4/3/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+import UIKit
+
+class GitHubAuthController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func printTokenPressed(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        let parameters = ["scope" : "email,user"]
+        
+        GitHub.shared.oAuthRequestWith(parameters: parameters)
+        
+    }
+    
+}
