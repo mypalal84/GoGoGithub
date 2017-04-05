3980ca108d35270b00f182e80afa43af9f42d7f0
Alex Cahn <alexcahn@gmail.com>
2017-04-05 09:02:12 -0700

    fixing gitignore please'

---
+++ GoGoGithub/GoGoGithub/RepoTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  RepoTableViewCell.swift
+//  GoGoGithub
+//
+//  Created by A Cahn on 4/4/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var repoNameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var languageLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
